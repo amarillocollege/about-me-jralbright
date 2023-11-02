295ed5aa59c6f3109ed660e8644bae44e537bafd
jralbright <148795844+jralbright@users.noreply.github.com>
2023-11-02 17:27:52 -0500

    Update About_MeApp.swift
    Had to update directly on Github since my new MacBook would not allow me to update from Xcode for some reason. Will ask for help in class on Monday with issues.

--- About Me/About_MeApp.swift
+++ About Me/About_MeApp.swift
@@ -1,16 +1,46 @@
 //
-//  About_MeApp.swift
+//  ContentView.swift
 //  About Me
 //
+//  Created by [Joseph Albright].
 //
 
 import SwiftUI
 
-@main
-struct About_MeApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+struct ContentView: View {
+    var body: some View {
+        
+        @State var message1 = "Hello, my name is Joseph Albright."
+        @State var message2 = "I enjoy painting miniatures."
+        
+        VStack {
+            Image(systemName: "book.pages")
+                .imageScale(.medium)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .foregroundStyle(.tint)
+                .scaledToFit()
+            Text(message1)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+            Text(message2)
+                .fontWeight(.medium)
+                .foregroundColor(.green)
+            
+            HStack{
+                Button("More Information"){
+                    message1 = "I am 34 years old and have a teaching bachelors and masters."
+                }
+                Button("More Hobbies"){
+                    message2 = "I also enjoy reading and playing video games."
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
         }
+        .padding()
     }
+}
+
+#Preview {
+    ContentView()
 }
