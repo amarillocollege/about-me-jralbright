ec1449a548f5c0e7cad8b5e20577c83de6f5d1eb
jralbright <148795844+jralbright@users.noreply.github.com>
2023-11-06 11:04:55 -0600

    Update About_MeApp.swift

--- About Me/About_MeApp.swift
+++ About Me/About_MeApp.swift
@@ -1,46 +1,17 @@
 //
-//  ContentView.swift
+//  About_MeApp.swift
 //  About Me
 //
-//  Created by [Joseph Albright].
+//  Created by AC Student on 11/6/23.
 //
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        
-        @State var message1 = "Hello, my name is Joseph Albright."
-        @State var message2 = "I enjoy painting miniatures."
-        
-        VStack {
-            Image(systemName: "book.pages")
-                .imageScale(.medium)
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                .foregroundStyle(.tint)
-                .scaledToFit()
-            Text(message1)
-                .fontWeight(.heavy)
-                .foregroundColor(.red)
-            Text(message2)
-                .fontWeight(.medium)
-                .foregroundColor(.green)
-            
-            HStack{
-                Button("More Information"){
-                    message1 = "I am 34 years old and have a teaching bachelors and masters."
-                }
-                Button("More Hobbies"){
-                    message2 = "I also enjoy reading and playing video games."
-                }
-            }
-            .buttonStyle(.borderedProminent)
-            
+@main
+struct About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
