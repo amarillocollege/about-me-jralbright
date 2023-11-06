ac2d098a5bb9f82b027896860e6790f9ab280f24
jralbright <148795844+jralbright@users.noreply.github.com>
2023-11-06 10:36:15 -0600

    Update ContentView.swift

--- About Me/ContentView.swift
+++ About Me/ContentView.swift
@@ -9,11 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        @State var message1 = "Hello, my name is Joseph Albright."
+        @State var message2 = "I enjoy painting miniatures."
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Image(systemName: "book.pages")
+                .imageScale(.medium)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .scaledToFit()
+            Text(message1)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+            Text(message2)
+                .fontWeight(.medium)
+                .foregroundColor(.green)
+            
+            HStack{
+                Button("More Information"){
+                    message1 = "I am 34 years old and have a teaching bachelors and masters."
+                }
+                Button("More Hobbies"){
+                    message2 = "I also enjoy reading and playing video games."
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
         }
         .padding()
     }
